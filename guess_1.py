1bb2f58d69f0bd4e1fbfed44f0cfb37ffe1b805c
xasterKies <samueledward40@gmail.com>
2024-07-03 21:52:11 +0100

    guess1 and guess2

---
+++ guess_1.py
@@ -0,0 +1,7 @@
+
+guess = 3
+
+if guess == 5:
+    print("True Path")
+else:
+    print("False Path")
---
+++ guess_2.py
@@ -0,0 +1,14 @@
+guess = 5
+
+guess = 3
+
+if guess == 5:
+    print("You win!")
+
+else:
+    if guess > 5:
+        print("Too high")
+    else:
+        print("Too low")
+
+print("Game over!")
